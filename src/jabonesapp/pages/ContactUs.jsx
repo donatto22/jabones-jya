3d2216f05eff621778ad87af3096feeef3f586aa
donatto22 <ottanod22@gmail.com>
2024-04-16 18:58:57 -0500

    Pequeño estilo agregado a la página de contacto

--- src/jabonesapp/pages/ContactUs.jsx
+++ src/jabonesapp/pages/ContactUs.jsx
@@ -1,9 +1,35 @@
+import { Box, Text,
+    FormControl,
+    FormLabel,
+    Input,
+    VStack, } from "@chakra-ui/react"
 import { JabonesLayout } from "../layout/JabonesLayout"
+
+const PersonalizedInput = ({ ...props }) => {
+    return <Input { ...props } border='0' borderRadius='4px' mt='10px' p='.4em 0' textIndent={10}/>
+}
 
 const ContactUs = () => {
     return (
         <JabonesLayout>
-            <div>ContactUs</div>
+            <Box w='100%' h='80dvh' display='flex' justifyContent='center' flexDir='column' alignItems='flex-start'>
+                <Box w='80%' m='0 auto'>
+                    <Text fontSize='30px' mt={0} boxShadow='0 2px 0 black' width='fit-content'>Contacto</Text>
+                    <Text fontSize='30px' mt={0} width='fit-content'>. . .</Text>
+
+                    {/* <VStack align='start'>
+                        <FormControl>
+                            <FormLabel>Correo</FormLabel>
+                            <PersonalizedInput type='email' />
+                        </FormControl>
+
+                        <FormControl>
+                            <FormLabel>Telefono</FormLabel>
+                            <PersonalizedInput type='number' />
+                        </FormControl>
+                    </VStack> */}
+                </Box>
+            </Box>
         </JabonesLayout>
     )
 }
