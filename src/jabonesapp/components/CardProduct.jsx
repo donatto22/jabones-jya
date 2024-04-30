4bae6e368274c15c42c5cc539db32cc41b910245
donatto22 <ottanod22@gmail.com>
2024-04-30 13:26:56 -0500

    Se agregan 2 productos en la pagina de productos

---
+++ src/jabonesapp/components/CardProduct.jsx
@@ -0,0 +1,33 @@
+import { Box, Button, Tooltip, VStack } from "@chakra-ui/react"
+import { FaShoppingBag } from "react-icons/fa"
+
+import PropTypes  from 'prop-types'
+
+export const CardProduct = ({ image, price, tooltipLabel }) => {
+    return (
+        <VStack bgImage={ image } bgPosition='center' bgSize='cover'
+        bgRepeat='no-repeat' width='50%'
+        bgColor='red' borderRadius='40px' height='500px'
+        justifyContent='space-between' alignItems='end'>
+            <Box color='var(--soap)' bgColor='var(--brown)' p='1em 1.6em'
+            borderRadius='0 40px' fontSize='20px'> S/. { price || '10' }</Box>
+
+            <Tooltip label={tooltipLabel} bgColor='rgba(0, 0, 0, .4)' p='10px 20px' color='var(--white)' borderRadius='20px' placement="top">
+                <Button cursor='pointer' p='.8em 0' m='0 auto 1em' leftIcon={<FaShoppingBag/>} 
+                width='90%' bgColor='rgba(0, 0, 0, .8)' borderRadius='40px'
+                color='var(--white)' border='0' sx={{
+                    '&:hover': {
+                        bgColor: 'rgba(0, 0, 0, .9)'
+                    }
+                }}>
+                    Comprar
+                </Button>
+            </Tooltip>
+        </VStack>
+    )
+}
+
+CardProduct.propTypes = {
+    price: PropTypes.string,
+    tooltipLabel: PropTypes.string
+}
--- src/jabonesapp/pages/Products.jsx
+++ src/jabonesapp/pages/Products.jsx
@@ -1,9 +1,24 @@
+import { Box, Stack, Text } from "@chakra-ui/react"
 import { JabonesLayout } from "../layout/JabonesLayout"
+
+import { CardProduct } from '../components/CardProduct'
+
+// imagenes
+import GlicerineSoapImage from '../../assets/glicerine_soap.png'
+import LavandaSoapImage from '../../assets/lavanda_soap.jpg'
  
 const Products = () => {
     return (
         <JabonesLayout>
-            <div>Products</div>
+            <Box width='70%' m='0 auto'>
+                <Text fontSize='40px'>
+                    Lo más solicitado
+                </Text>
+                <Stack direction='row' gap='4em'>
+                    <CardProduct price='12' image={GlicerineSoapImage} tooltipLabel='Jabón de glicerina' />
+                    <CardProduct price='13' image={LavandaSoapImage} tooltipLabel='Jabón de lavanda'/>
+                </Stack>
+            </Box>
         </JabonesLayout>
     )
 }
